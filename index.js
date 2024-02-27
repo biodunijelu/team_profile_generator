342133e6d68d0ed3b205078600d5c259727f1937
BiodunIjelu <biodunijelu@gmail.com>
2024-02-27 12:03:08 +0100

    Function to prompt user to input Manager's details

--- index.js
+++ index.js
@@ -11,3 +11,34 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const teamMembers = [];
+
+// Function to prompt user to input Manager's details
+function promptManager() {
+    console.log("Please enter the details of the team manager:");
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Manager's name:",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Manager's employee ID:",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Manager's email:",
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Manager's office number:",
+        }
+    ]).then(answers => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        teamMembers.push(manager);
+        promptEmployeeType();
+    });
+}
